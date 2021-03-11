6c42c741f7c5c11d2600bfee2c27688839f44f7e
Kingloo <k1ngloo@hotmail.com>
2021-03-11 06:49:07 +0100

    spiffy-rs

---
+++ spiffy-rs/src/main.rs
@@ -0,0 +1,41 @@
+use reqwest::header::USER_AGENT;
+use reqwest::Method;
+use reqwest::Url;
+
+fn main() {
+    let ipv4_endpoint = Url::parse("https://api.ipify.org/").expect("failed to parse IPv4 endpoint");
+    let ipv6_endpoint = Url::parse("https://api6.ipify.org/").expect("failed to parse IPv6 endpoint");
+
+    let ipv4 = get_ip_address(ipv4_endpoint);
+    let ipv6 = get_ip_address(ipv6_endpoint);
+
+    match ipv4 {
+        Some(address) => println!("your IPv4 address is {}", address),
+        None => println!("failed to get IPv4 address")
+    };
+
+    match ipv6 {
+        Some(address) => println!("your IPv6 address is {}", address),
+        None => println!("failed to get IPv6 address")
+    };
+}
+
+fn get_ip_address(url: Url) -> Option<String> {
+    let mut result: Option<String> = None;
+
+    let mut request = reqwest::blocking::Request::new(Method::GET, url);
+    let headers = request.headers_mut();
+    headers.insert(USER_AGENT, "spiffy-rs (https://github.com/Kingloo/spiffy-rs)".parse().expect("failed to parse User-Agent"));
+
+    let request_url: Url = request.url().clone();
+
+    if let Ok(r) = reqwest::blocking::get(request_url) {
+        if r.status().is_success() {
+            if let Ok(s) = r.text() {
+                result = Some(s);
+            }
+        }
+    }
+
+    result
+}
