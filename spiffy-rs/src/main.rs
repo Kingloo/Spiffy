0ce47ee84ed7c2f800e0e575bac37e40e90a6a98
Kingloo <k1ngloo@hotmail.com>
2021-03-11 07:09:38 +0100

    Pretty print command line arg
    Pass --pretty as arg for the response to be human-readable.

--- spiffy-rs/src/main.rs
+++ spiffy-rs/src/main.rs
@@ -3,19 +3,31 @@ use reqwest::Method;
 use reqwest::Url;
 
 fn main() {
-    let ipv4_endpoint = Url::parse("https://api.ipify.org/").expect("failed to parse IPv4 endpoint");
-    let ipv6_endpoint = Url::parse("https://api6.ipify.org/").expect("failed to parse IPv6 endpoint");
+    let args: Vec<String> = std::env::args().collect();
+    let pretty_print = args.contains(&String::from("--pretty"));
+
+    let ipv4_endpoint = Url::parse("https://api.ipify.org/")
+        .expect("failed to parse IPv4 endpoint");
+    
+        let ipv6_endpoint = Url::parse("https://api6.ipify.org/")
+        .expect("failed to parse IPv6 endpoint");
 
     let ipv4 = get_ip_address(ipv4_endpoint);
     let ipv6 = get_ip_address(ipv6_endpoint);
 
     match ipv4 {
-        Some(address) => println!("your IPv4 address is {}", address),
+        Some (address) => match pretty_print {
+            true => println!("your IPv4 address is {}", address),
+            false => println!("{}", address)
+        },
         None => println!("failed to get IPv4 address")
     };
 
     match ipv6 {
-        Some(address) => println!("your IPv6 address is {}", address),
+        Some(address) => match pretty_print {
+            true => println!("your IPv6 address is {}", address),
+            false => println!("{}", address)
+        },
         None => println!("failed to get IPv6 address")
     };
 }
@@ -25,7 +37,9 @@ fn get_ip_address(url: Url) -> Option<String> {
 
     let mut request = reqwest::blocking::Request::new(Method::GET, url);
     let headers = request.headers_mut();
-    headers.insert(USER_AGENT, "spiffy-rs (https://github.com/Kingloo/spiffy-rs)".parse().expect("failed to parse User-Agent"));
+    headers.insert(USER_AGENT, "spiffy-rs (https://github.com/Kingloo/spiffy-rs)"
+        .parse()
+        .expect("failed to parse User-Agent"));
 
     let request_url: Url = request.url().clone();
 
