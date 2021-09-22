606f39bf9fb915d9200687b46d8029c79c7d24cf
Kingloo <k1ngloo@hotmail.com>
2021-09-22 10:24:08 +0200

    Version bump
    Add editorconfig

--- spiffy-rs/src/main.rs
+++ spiffy-rs/src/main.rs
@@ -1,45 +1,45 @@
 use reqwest::Url;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    let pretty_print = args.contains(&String::from("--pretty"));
+	let args: Vec<String> = std::env::args().collect();
+	let pretty_print = args.contains(&String::from("--pretty"));
 
-    let ipv4_endpoint = Url::parse("https://api.ipify.org/")
-        .expect("failed to parse IPv4 endpoint");
-    
-    let ipv6_endpoint = Url::parse("https://api6.ipify.org/")
-        .expect("failed to parse IPv6 endpoint");
+	let ipv4_endpoint = Url::parse("https://api.ipify.org/")
+		.expect("failed to parse IPv4 endpoint");
 
-    let ipv4 = get_ip_address(ipv4_endpoint);
-    let ipv6 = get_ip_address(ipv6_endpoint);
+	let ipv6_endpoint = Url::parse("https://api6.ipify.org/")
+		.expect("failed to parse IPv6 endpoint");
 
-    match ipv4 {
-        Some (address) => match pretty_print {
-            true => println!("your IPv4 address is {}", address),
-            false => println!("{}", address)
-        },
-        None => println!("failed to get IPv4 address")
-    };
+	let ipv4 = get_ip_address(ipv4_endpoint);
+	let ipv6 = get_ip_address(ipv6_endpoint);
 
-    match ipv6 {
-        Some(address) => match pretty_print {
-            true => println!("your IPv6 address is {}", address),
-            false => println!("{}", address)
-        },
-        None => println!("failed to get IPv6 address")
-    };
+	match ipv4 {
+		Some(address) => match pretty_print {
+			true => println!("your IPv4 address is {}", address),
+			false => println!("{}", address),
+		},
+		None => println!("failed to get IPv4 address"),
+	};
+
+	match ipv6 {
+		Some(address) => match pretty_print {
+			true => println!("your IPv6 address is {}", address),
+			false => println!("{}", address),
+		},
+		None => println!("failed to get IPv6 address"),
+	};
 }
 
 fn get_ip_address(url: Url) -> Option<String> {
-    let mut result: Option<String> = None;
+	let mut result: Option<String> = None;
 
-    if let Ok(r) = reqwest::blocking::get(url) {
-        if r.status().is_success() {
-            if let Ok(s) = r.text() {
-                result = Some(s);
-            }
-        }
-    }
+	if let Ok(r) = reqwest::blocking::get(url) {
+		if r.status().is_success() {
+			if let Ok(s) = r.text() {
+				result = Some(s);
+			}
+		}
+	}
 
-    result
+	result
 }
